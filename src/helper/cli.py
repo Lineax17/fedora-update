20cd4dfc67b2c8bf98957d2910f85f59410c83db
Lineax17 <lineax17@gmail.com>
2025-12-23 19:55:34 +0100

    cli.py init

--- src/helper/cli.py
+++ src/helper/cli.py
@@ -1,30 +1,8 @@
-def version(version_number: str) -> None:
-    """Prints the version number of the application."""
-    print(f"Fedora Update Control Kit: {version_number}")
+def print_output(function, verbose_mode: bool):
+    if verbose_mode:
+        function()
+    else:
+        run_with_spinner(function())
 
-def help() -> None:
-    """Prints help information for the application."""
-    help_text = """
-    Usage: fedora-update (fedora-upgrade|fuck) [-l|--log|--verbose] [-b|--brew|--brew-upgrade]
-
-    Automated system upgrade script for Fedora Linux.
-
-    Options:
-        -l, --log, --verbose       Show detailed output during upgrade process
-        -b, --brew, --brew-upgrade Update Homebrew packages (if installed)
-        -h, --help                 Display this help message
-        -v, --version              Show current version of tool
-
-    Examples:
-        fedora-update              # Run in silent mode with spinner
-        fedora-update -l           # Run in verbose mode with detailed output
-        fedora-update -b           # Update Homebrew packages
-
-    Aliases:
-        fedora-upgrade, fuck       # Symlinks to this script; behave identically
-
-    Note:
-        Multiple options can be combined. The -b flag only runs if Homebrew is installed.
-    """
-    print(help_text)
-
+def run_with_spinner(command):
+    pass
