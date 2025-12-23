6574ad2d2e0af1fcef420c68e5903c558234fe5c
Lineax17 <lineax17@gmail.com>
2025-12-23 20:15:32 +0100

    add flatpak install check

--- src/core/flatpak.py
+++ src/core/flatpak.py
@@ -1,5 +1,16 @@
 from helper import runner
 
+def check_flatpak_installed() -> bool:
+    """Check if Flatpak is installed on the system."""
+    try:
+        runner.run(["flatpak", "--version"])
+        return True
+    except runner.CommandError:
+        return False
+
 def update_flatpak():
     """Update Flatpak packages."""
-    runner.run(["flatpak", "update"])
+    if not check_flatpak_installed():
+        print("Flatpak is not installed on this system.")
+    else:
+        runner.run(["flatpak", "update"])
