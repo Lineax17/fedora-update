852676d198d22bd8fa755971e7dc2b9209813308
Lineax17 <lineax17@gmail.com>
2025-12-27 18:44:58 +0100

    try to fix brew not in path bug

--- src/core/brew.py
+++ src/core/brew.py
@@ -3,7 +3,7 @@
 This module provides functions to check Homebrew availability and update
 installed Homebrew packages and casks.
 """
-
+import os
 from helper import runner
 
 def _check_brew_installed() -> bool:
@@ -13,7 +13,7 @@ def _check_brew_installed() -> bool:
         True if Homebrew is available, False otherwise.
     """
     try:
-        result = runner.run(["brew", "--version"], check=False)
+        result = runner.run(["bash", "-c", "command -v brew"], check=False)
         return result.returncode == 0
     except FileNotFoundError:
         return False
@@ -31,8 +31,8 @@ def update_brew(show_live_output: bool = False) -> str | None:
     if not _check_brew_installed():
         return "Homebrew is not installed on this system."
     else:
-        runner.run(["brew", "update"], show_live_output=show_live_output)
-        runner.run(["brew", "upgrade"], show_live_output=show_live_output)
+        runner.run(["bash", "-c", "brew", "update"], show_live_output=show_live_output)
+        runner.run(["bash", "-c", "brew", "upgrade"], show_live_output=show_live_output)
         return None
 
 
