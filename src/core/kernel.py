64e5aae0b4d3f9bd7add9c67adbc6283ebefead3
Lineax17 <lineax17@gmail.com>
2025-12-27 12:49:12 +0100

    implement check_kernel_update

--- src/core/kernel.py
+++ src/core/kernel.py
@@ -1,5 +1,17 @@
+from helper import runner
+
+
 def new_kernel_version() -> bool:
-    pass
+    new_kernel_version_available: bool
+    result = runner.run(["dnf5", "check-upgrade", "-q", "kernel*"], check=False)
+    if result.returncode == 0:
+        new_kernel_version_available = False
+    elif result.returncode == 100:
+        new_kernel_version_available = True
+    else:
+        raise runner.CommandError("Kernel update check failed")
+
+    return new_kernel_version_available
 
 def get_new_kernel_version() -> str:
     pass
